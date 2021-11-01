4bf36e8cd105d06ae3d7259b7bf84a0211f7f554
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-11-01 20:47:17 -0300

    Mutation - Salvar Cliente e Deletar Cliente

--- compras/src/main/java/com/udemy/compras/ClienteGraphQL.java
+++ compras/src/main/java/com/udemy/compras/ClienteGraphQL.java
@@ -1,13 +1,15 @@
 package com.udemy.compras;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
-public class ClienteGraphQL implements GraphQLQueryResolver {
+public class ClienteGraphQL implements GraphQLQueryResolver, GraphQLMutationResolver {
 
     @Autowired
     private ClienteRepository clienteRepository;
@@ -18,5 +20,19 @@ public class ClienteGraphQL implements GraphQLQueryResolver {
 
     public List<Cliente> getClientes(){
         return clienteRepository.findAll();
+    }
+
+    public Cliente saveCliente(Long id, String nome, String email){
+        Cliente c = new Cliente(id, nome, email);
+        return clienteRepository.save(c);
+    }
+
+    public Boolean deletarCliente(Long id){
+        Optional<Cliente> clienteInTheDatabase = clienteRepository.findById(id);
+        if (!clienteInTheDatabase.isPresent()){
+            return false;
+        }
+        clienteRepository.deleteById(id);
+        return true;
     }
 }
